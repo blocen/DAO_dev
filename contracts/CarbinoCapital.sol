57748625a8d430de5503c59f5d1d3e45c16c9268
Raphael <89421168+blocen@users.noreply.github.com>
2021-11-13 09:57:26 +0100

    CC token extended

--- contracts/CarbinoCapital.sol
+++ contracts/CarbinoCapital.sol
@@ -1,20 +1,41 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.7;
 
-// import "./ERC20.sol"
-// contract ERC20Detailed is ERC20.sol
-
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 
-contract CarbinoCapital is ERC20Votes {
+contract CarbinoCapital is ERC20, ERC20Permin, ERC20Votes, ERC20Wrapper {
     uint256 public maxSupply = 1000 * 10 ** 18;
 
-    constructor()
+    constructor(IERC20 wrappedToken)
         ERC20("CarbinoCapital", "CC")
         ERC20Permit("CarbinoCapital")
+        ERC20Wrapper(wrappedToken)
     {
         _mint(msg.sender, maxSupply);
     }
 
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
 }
